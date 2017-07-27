39b49dae46b7b0921f523600278207a0dd36126b
EliGarcia <garciaelisha93@yahoo.com>
2017-07-27 14:34:07 -0500

    DONE bbyyyyyyyyyyyy

--- script.js
+++ script.js
@@ -84,7 +84,9 @@ function addActor() {
 */
 function getLosses() {
   // Your answer here:
+  return film.boxoffice - film.budget
 }
+
 
 
 
@@ -103,6 +105,11 @@ let letterVals = ["v", "x", "r", "f", "p"];
 function interleave() {
   let vals = [];
   // Your answer here:
+  for (var i = 0; i < numberVals.length; i++) {
+    vals.push(letterVals[i] + numberVals[i])
+  }
+  return vals;
+
 }
 
 
@@ -120,4 +127,10 @@ let second = ["think", "stand", "cheese", "break", "numinous", "mouse", "close",
 function union() {
   let same = [];
   // Your answer here:
+  for (var i = 0; i < first.length; i++) {
+    if(first[i]===second[i]){
+      same.push(first[i])
+    }
+  }
+  return same;
 }
